e889563ef1a5cf8b183200b420c85b171ef08fa3
Pablo Alejandro <pabloabur@gmail.com>
2016-11-02 23:33:39 -0200

    some fixes on reading input files

--- loading.r
+++ loading.r
@@ -8,19 +8,22 @@ file_path <- paste ("yelp_academic_dataset_", file_name, ".json", sep = "")
 path <- file_path
 
 # Geting variables of interest
-# reads first line to get types and names
+# reads first line to get names
 aux_attributes <- fromJSON (readLines (file (path), 1, encoding="UTF-8"))
 print (names (aux_attributes))
-ans<-readline ("Insert the attribute names you want, as above and separated by space:")
-attributes <- as.vector(strsplit(ans, " ")[[1]])
+ans <- readline ("Insert the attribute names you want, as above and separated by space:")
+# [[1]] used to get data itself from strplit output
+attributes <- as.vector(strsplit(ans, " ")[[1]]) 
 
-con_in <- file (path)
 con_out <- file (paste ("file_", file_name, sep = ""), open = "wb")
 
 # Each line in document is a JSON object, so stream is used
-stream_in (con_in, handler = function (df) {
-                chunk <- subset (df, select = which (names (df) %in% attributes))
-                stream_out (chunk, con_out)
-}, pagesize = 500000)
+stream_in (file (path), handler = function (df) {
+    # Subset all rows, which return columns indexes 
+    # %in% generates logical vector with T where names where found
+    chunk <- df[,which (names (df) %in% attributes)]
+    # Writes to output
+    stream_out (chunk, con_out)
+}, pagesize = 50000) # Avoid using too large pagesizes
 close(con_out)
 
