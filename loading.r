6bdf2f6f0ca8519fb178b37c5db891ad5eb100e7
Pablo Alejandro <pabloabur@gmail.com>
2016-12-09 23:22:35 -0200

    Improvements on loading and added feature for user city on vegan

--- loading.r
+++ loading.r
@@ -2,28 +2,35 @@ library (jsonlite)
 
 # Obtaining path to file from an input
 file_name <- readline ("review, business, checkin, tip or user? ")
-file_path <- paste ("yelp_academic_dataset_", file_name, ".json", sep = "")
-# Next line for a more structured structure
-# path <- file.path (getwd (), "Dataset", file_path)
-path <- file_path
+path <- paste ("yelp_academic_dataset_", file_name, ".json", sep = "")
 
 # Geting variables of interest
 # reads first line to get names
 aux_attributes <- fromJSON (readLines (file (path), 1, encoding="UTF-8"))
 print (names (aux_attributes))
 ans <- readline ("Insert the attribute names you want, as above and separated by space:")
-# [[1]] used to get data itself from strplit output
-attributes <- as.vector(strsplit(ans, " ")[[1]]) 
+# Get data itself from strplit output, which returns list
+attributes <- unlist (strsplit (ans, " ")) 
 
-con_out <- file (paste ("file_", file_name, sep = ""), open = "wb")
+con_name <- paste ("file_", file_name, sep = "")
+con_out <- file (con_name, open = "wb")
 
 # Each line in document is a JSON object, so stream is used
 stream_in (file (path), handler = function (df) {
     # Subset all rows, which return columns indexes 
     # %in% generates logical vector with T where names where found
+    # TODO use dplyr
     chunk <- df[,which (names (df) %in% attributes)]
     # Writes to output
-    stream_out (chunk, con_out)
+    stream_out (chunk, con_out, pagesize = 10000)
 }, pagesize = 50000) # Avoid using too large pagesizes
 close(con_out)
 
+# Recover generated data and save it as a single object
+print ("Saving .rds...")
+gen_data <- stream_in (file (con_name))
+saveRDS (gen_data, paste (file_name, ".rds", sep = ""))
+
+# Remove variables and files used
+unlink (con_name)
+rm (list = ls ())
--- vegan.r
+++ vegan.r
@@ -1,7 +1,15 @@
 library (dplyr) # Intended to be dplyr-like code
 
-load ("businesses.rda")
-load ("reviews.rda")
+# A Mode function will be needed TODO make your own
+Mode <- function(x) {
+    ux <- unique(x)
+    ux[which.max(tabulate(match(x, ux)))]
+}
+
+businesses <- readRDS ("business.rds")
+reviews <- readRDS ("review.rds")
+#load ("businesses.rda")
+#load ("reviews.rda")
 
 # Filter matches with "vegan" in businesses
 businesses <- mutate (businesses, is_vegan = grepl ("vegan", categories, ignore.case=T))
@@ -9,8 +17,8 @@ vegan_businesses <- filter (businesses, is_vegan)
 
 # Get reviews of vegan businesses and then identify possible vegans
 vegan_reviews <- filter (reviews, business_id %in% vegan_businesses$business_id)
+# TODO maybe_vegan_user <- distinct (vegan_reviews, user_id)
 maybe_vegan_user <- unique (vegan_reviews$user_id)
-# maoi<-inner_join(vegan_reviews,select(businesses,business_id,city))
 
 # Next steps are used to identify vegans
 
@@ -34,15 +42,33 @@ vegan_data <- full_join(not_vegan_subset, vegan_subset)
 vegan_data$qt_not_vegan_review[is.na (vegan_data$qt_not_vegan_review)] <- 0
 
 # Apply criteria to be classified as vegan
-vegans<-filter(vegan_data,qt_vegan_review>1)
+vegans <- filter (vegan_data, qt_vegan_review > 1, qt_vegan_review > qt_not_vegan_review)
 
-vegan_states<-inner_join(reviews,select(vegans,user_id))
+# To find out user city, start by getting city of review
+# There is 'user_id' in vegan_reviews and 'city' in businesses
+review_city <- inner_join (vegan_reviews, businesses)
+user_city <- review_city %>%
+    select (user_id, city) %>%
+    group_by (user_id) %>%
+    summarise (cities = list (city)) %>%
+    mutate (city = sapply (cities, Mode)) %>%
+    select (-cities)
+# Join with vegans dataset
+vegans <- inner_join (vegans, user_city)
+# TODO check with review city
+# zz4iaqRAIT-wju1J8wicgA
+#review_city[which(review_city$user_id=="zz4iaqRAIT-wju1J8wicgA"),]
 
-dotchart(table(businesses$city))
-cities<-data.frame(city=c("Las Vegas","Phoenix","Charlotte","Scottsdale","Montréal"))
-bsn_ct<-inner_join(businesses,cities)
-vegan_states<-inner_join(vegan_states,bsn_ct)
-tabela1<-data.frame(Cidade=c("Las Vegas","Phoenix","Charlotte","Scottsdale","Montréal"))
-sum(user_city$city == "Phoenix")
-tabela1$Qtd_Veganos<-c("963","1060","140","272","218")
-
+#vegan_states<-inner_join(reviews,select(vegans,user_id))
+#
+#dotchart(table(businesses$city))
+#cities<-data.frame(city=c("Las Vegas","Phoenix","Charlotte","Scottsdale","Montréal"))
+#bsn_ct<-inner_join(businesses,cities)
+#vegan_states<-inner_join(vegan_states,bsn_ct)
+#tabela1<-data.frame(Cidade=c("Las Vegas","Phoenix","Charlotte","Scottsdale","Montréal"))
+##sum(user_city$city == "Phoenix")
+#tabela1$Qtd_Veganos<-c("963","1060","140","272","218")
+#
+# Remove unnecessary objects
+rm (businesses, reviews, maybe_vegan_user, vegan_reviews, not_vegan_subset,
+    vegan_subset, all_other_reviews, vegan_data, review_city, user_city)
